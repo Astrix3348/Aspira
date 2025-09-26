e3af4250f0092039abd36f2a98336d77cf52ea19
Aakash Gangwar <aakashgangwar@Aakashs-MacBook-Air.local>
2025-09-26 21:12:47 +0530

    added placeholder and we can now sign in and sign up and it redirects to home

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,6 +8,8 @@ import { Button } from "@/components/ui/button"
 import {Form,} from "@/components/ui/form"
 import Link from "next/link";
 import {toast} from "sonner";
+import FormField from "@/components/FormField";
+import {useRouter} from "next/navigation";
 
 
 const authFormSchema = (type : FormType) => {
@@ -21,6 +23,7 @@ const authFormSchema = (type : FormType) => {
 
 
 const AuthForm = ({type} : { type : FormType}) => {
+    const router = useRouter();
     const formSchema = authFormSchema(type);
     const form = useForm<z.infer<typeof formSchema>>({
         
@@ -36,10 +39,12 @@ const AuthForm = ({type} : { type : FormType}) => {
     function onSubmit(values: z.infer<typeof formSchema>) {
        try{
            if(type === "sign-up"){
-               console.log('SIGN UP', values);
+               toast.success("Account successfully created!. Please Sign In");
+               router.push("/sign-in");
            }
            else{
-               console.log('SIGN IN', values);
+               toast.success("Sign In Successfully!");
+               router.push("/");
            }
        }catch(error){
            console.log("error");
@@ -67,9 +72,29 @@ const AuthForm = ({type} : { type : FormType}) => {
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} 
                       className="w-full space-y-6 mt-4 form">
-                    {!isSignIn && <p>Name</p>}
-                    <p>Email</p>
-                    <p>Password</p>
+                    {!isSignIn && (
+                        <FormField 
+                            control={form.control}
+                            name="name"
+                            label="Name"
+                            placeholder="Your Name"  
+                        />
+                    )}
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="Your Email Address"
+                        type="email"
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        label="Password"
+                        placeholder="Enter Your Password"
+                        type="password"
+                    />
                     
                     <Button className="btn"
                             type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
